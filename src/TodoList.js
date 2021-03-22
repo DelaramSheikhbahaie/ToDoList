c3d7203114439ab4b6a5c89363aca5e84dbc8c67
DelaramSheikhbahaie <sheikhbahaied@gmail.com>
2021-03-22 19:40:51 +0430

    buttons are edited

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,9 +3,18 @@ import React from 'react';
 const TodoList = (props) => {
     return (
      <div className="todos">
-      <span className="todo-field">{props.listItem.Text}</span>
-      <button className="button-primary check-btn"  onClick={() => props.onCheck(props.listItem)}>Compeleted</button>
-      <button className="button-primary delete-btn" onClick={() => props.onDelete(props.listItem.id)}>Delete</button>
+      <span className={ `todo-field ${props.listItem.completed ? "completed" : ""}`}>
+      {props.listItem.Text} </span>
+
+      <button className="button-primary check-btn" 
+       onClick={() => props.onCheck(props.listItem)}>
+         <i class="fas fa-check"></i>
+         </button>
+
+      <button className="button-primary delete-btn "
+       onClick={() => props.onDelete(props.listItem.id)}>
+         <i class="fas fa-trash"></i>
+         </button>
     </div>
       );
 }
